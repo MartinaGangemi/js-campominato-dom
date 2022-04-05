44d0ce06c29b069bf6295098e079e73547ff8c16
MartinaGangemi <martinagangemi94@gmail.com>
2022-04-05 14:16:34 +0200

    ticket

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,18 +10,23 @@ console.log(difficoltà)
 if (difficoltà == "facile"){
     createGrid("cell", "cell_easy_mode", 100)
     coloredSquare (".cell", "active")
+    generateBombs (100)
      
     } else if(difficoltà == "media"){
         createGrid("cell", "cell_normal_mode", 81)
         coloredSquare (".cell", "active")
+        generateBombs (81)
         
     } else {
         createGrid("cell", "cell_hard_mode", 49)
         coloredSquare (".cell", "active")
+        generateBombs (100)
     }
 }
 
 // creo la funzione per generare le celle
+
+
 function createGrid(className, secondClassName, numeroDifficoltà){
     const cellsElement = document.getElementById("cells")
 
@@ -32,37 +37,44 @@ function createGrid(className, secondClassName, numeroDifficoltà){
     divTag.classList.add(secondClassName)
     cellsElement.append(divTag)
     divTag.append(i)
-    } 
+   } 
 }
+
+
 
 //funzione quando clicco sul quadrato si colora
 function coloredSquare (selector, activeClass){
 const arrayCell = document.querySelectorAll(selector);
-for (let i = 0; i<arrayCell.length;i++){
-    const quadratino = arrayCell[i]
+for (let k = 0; k<arrayCell.length;k++){
+    const quadratino = arrayCell[k]
     quadratino.addEventListener("click", function (){
-        console.log(this, i)
+        console.log(this, k)
         this.classList.add(activeClass)
      })
     }     
 }
+
+
+
 
 // creo 16 numeri random
 function randomNumbers(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
+// creo una funzione per geenerare le bombe
 
-
+function generateBombs (numerocelle) {
 const randomBomb = [];
 while (randomBomb.length !==16){
-     const bomb = randomNumbers (1, 16)
+     const bomb = randomNumbers (1, numerocelle)
      if(!randomBomb.includes(bomb)){
          randomBomb.push(bomb)
      }
  }
 
  console.log(randomBomb)
+}
 
 
 
