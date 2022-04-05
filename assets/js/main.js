438c35a55fc20f2ce120943f7deb1086ef973e15
MartinaGangemi <martinagangemi94@gmail.com>
2022-04-05 16:47:36 +0200

    ticket

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,19 +9,21 @@ console.log(difficoltà)
 
 if (difficoltà == "facile"){
     createGrid("cell", "cell_easy_mode", 100)
-    coloredSquare (".cell", "active")
+    coloredSquare (".cell", "active", "active_bomb")
     generateBombs (100)
      
     } else if(difficoltà == "media"){
         createGrid("cell", "cell_normal_mode", 81)
-        coloredSquare (".cell", "active")
+        coloredSquare (".cell", "active", "active_bomb" )
         generateBombs (81)
         
     } else {
         createGrid("cell", "cell_hard_mode", 49)
-        coloredSquare (".cell", "active")
-        generateBombs (100)
+        coloredSquare (".cell", "active" , "active_bomb")
+         generateBombs (100)
     }
+
+
 }
 
 // creo la funzione per generare le celle
@@ -31,30 +33,33 @@ function createGrid(className, secondClassName, numeroDifficoltà){
     const cellsElement = document.getElementById("cells")
 
     // ciclo per generare tot quadratini
-    for (let i = 1; i<=numeroDifficoltà; i++){
+    for (let i = 1; i<=numeroDifficoltà; ++i){
     const divTag = document.createElement("div")
     divTag.classList.add(className)
     divTag.classList.add(secondClassName)
     cellsElement.append(divTag)
     divTag.append(i)
-   } 
+    } 
 }
-
-
 
 //funzione quando clicco sul quadrato si colora
-function coloredSquare (selector, activeClass){
+function coloredSquare (selector, activeClass, activeBomb){
 const arrayCell = document.querySelectorAll(selector);
-for (let k = 0; k<arrayCell.length;k++){
+for (let k = 1 ; k<arrayCell.length ; k++){
     const quadratino = arrayCell[k]
     quadratino.addEventListener("click", function (){
-        console.log(this, k)
+    
+    console.log(this,k)
+
+     if(randomBomb.includes (k)){
+         this.classList.add(activeBomb)
+     } else{
         this.classList.add(activeClass)
-     })
+     }
+
+    })
     }     
 }
-
-
 
 
 // creo 16 numeri random
@@ -62,19 +67,23 @@ function randomNumbers(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
+
+let randomBomb = [];
+console.log(randomBomb)
 // creo una funzione per geenerare le bombe
 
 function generateBombs (numerocelle) {
-const randomBomb = [];
-while (randomBomb.length !==16){
-     const bomb = randomNumbers (1, numerocelle)
-     if(!randomBomb.includes(bomb)){
-         randomBomb.push(bomb)
-     }
- }
 
- console.log(randomBomb)
+while (randomBomb.length !==16){
+     bomb = randomNumbers (1, numerocelle)
+     if(!randomBomb.includes(bomb)){
+        randomBomb.push(bomb)
+        console.log(bomb)
+    }
 }
+
+}
+
 
 
 
